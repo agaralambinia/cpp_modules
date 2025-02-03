6071410406d0c2f6b4b179c5ad20d872450ab861
Daria Efimova <agaralambinia@yandex.ru>
2025-02-03 20:51:51 +0400

    done cpp09/2

---
+++ cpp_09/ex02/PmergeMe.hpp
@@ -0,0 +1,33 @@
+#ifndef PMERGEME_HPP
+#define PMERGEME_HPP
+
+#include <iostream>
+#include <vector>
+#include <list>
+#include <ctime>
+#include <iomanip>
+#include <cstdlib>
+#include <string>
+#include <cctype>
+
+class PmergeMe {
+private:
+    std::vector<int> _vec;
+    std::list<int> _lst;
+
+	bool isValidNumber(const char* str);
+	void mergeVector(std::vector<int>& arr, size_t left, size_t mid, size_t right);
+	void mergeSortVector(std::vector<int>& arr, size_t left, size_t right);
+	std::list<int> mergeList(std::list<int>& left, std::list<int>& right);
+	std::list<int> mergeSortList(std::list<int>& lst);
+
+public:
+    PmergeMe();
+    PmergeMe(const PmergeMe& other);
+    PmergeMe& operator=(const PmergeMe& other);
+    ~PmergeMe();
+
+	void sort(int argc, char** argv);
+};
+
+#endif
